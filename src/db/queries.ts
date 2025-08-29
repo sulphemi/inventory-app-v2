3367a8f3fae6beb303dcfd8b8bbf0215f7ee88aa
Jing Qian <jqian2272@gmail.com>
2025-08-29 04:43:16 -0400

    include more information in keyboard listing

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -1,7 +1,19 @@
 import pool from "./pool.js";
 
 async function getAllKeyboards() {
-    const { rows } = await pool.query("SELECT * FROM keyboards");
+    const query = `
+SELECT
+    k.keyboardID,
+    k.keyboardName,
+    m.manufacturerName,
+    s.switchName,
+    k.quantity
+FROM keyboards AS k
+JOIN manufacturers AS m
+ON k.manufacturerID = m.manufacturerID
+JOIN switches AS s ON s.switchID = k.switchID
+`;
+    const { rows } = await pool.query(query);
     console.log(rows);
     return rows;
 }
