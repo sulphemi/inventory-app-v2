b7d1b825bb4014816c9723eb71d622124a63ab63
Jing Qian <jqian2272@gmail.com>
2025-11-22 08:05:16 -0500

    feat: queries for updating and deleting rows

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -131,10 +131,78 @@ LEFT JOIN item_status ist ON i.status_id = ist.id
         addendum,
     ];
 
-    console.log(inboundDate);
+    const res = await pool.query(query, values);
+    return res.rows[0];
+}
+
+async function editItem(
+    internalID: number,
+    warehouseID: string | null,
+    sku: string | string | null,
+    size: string | null,
+    notes: string | null,
+    quantity: number | null,
+    condition_id: number | null,
+    inboundDate: string | null,
+    outboundDate: string | null,
+    status_id: number | null,
+    addendum: string | null
+) {
+    const query = `
+WITH updated_item AS (
+    UPDATE items 
+    SET 
+        warehouseid = $1, 
+        sku = $2, 
+        size = $3, 
+        notes = $4, 
+        quantity = $5, 
+        condition_id = $6, 
+        inbounddate = $7, 
+        outbounddate = $8,
+        status_id = $9,
+        addendum = $10
+    WHERE internalID = $11
+    RETURNING *
+)
+SELECT 
+    i.internalID,
+    i.warehouseID,
+    i.sku,
+    i.size,
+    i.notes,
+    i.quantity,
+    ic.condition,
+    i.inboundDate,
+    i.outboundDate,
+    ist.status,
+    i.addendum
+FROM updated_item i
+LEFT JOIN item_conditions ic ON i.condition_id = ic.id
+LEFT JOIN item_status ist ON i.status_id = ist.id
+`;
+
+    const values = [
+        warehouseID,
+        sku,
+        size,
+        notes,
+        quantity,
+        condition_id,
+        inboundDate,
+        outboundDate,
+        status_id,
+        addendum,
+        internalID
+    ];
 
     const res = await pool.query(query, values);
     return res.rows[0];
+}
+
+async function deleteItem(internalID: number) {
+    const query = `DELETE FROM items WHERE internalID = $1`;
+    await pool.query(query, [internalID]);
 }
 
 async function suggestSKU(partialSKU: string) {
@@ -152,5 +220,7 @@ export default {
     newCondition,
     getAllStatuses,
     newItem,
+    editItem,
+    deleteItem,
     suggestSKU,
 };
