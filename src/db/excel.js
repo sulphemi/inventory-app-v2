d9229b83a28fb1eb621e7edb6b2820c6ff297249
Jing Qian <jqian2272@gmail.com>
2026-02-21 09:54:54 -0500

    wip: conditional formatting for excel sheet

--- src/db/excel.js
+++ src/db/excel.js
@@ -20,7 +20,8 @@ function monthly_summary(stream, data, enddate) {
         { header: "cnd", key: "condition", width: 20 },         // F
         { header: "in", key: "inbounddate", width: 20 },        // G
         { header: "out", key: "outbounddate", width: 20 },      // H
-        { header: "dc", key: "daycount", width: 10 },           // I
+        { header: "stat", key: "status", width: 20 },           // I
+        { header: "dc", key: "daycount", width: 10 },           // J
     ];
 
     for (const row of data) {
@@ -36,19 +37,29 @@ function monthly_summary(stream, data, enddate) {
 
     // apply daycount
     // first set master cell to first row, and slave cells all the way down
-    worksheet.getCell("I2").value = {
+    worksheet.getCell("J2").value = {
         formula: '=IF(AND(G2<=$K$1, G2<>""), IF(EOMONTH(G2,0)=EOMONTH($K$1,0), $K$1-G2, DAY($K$1)), 0)',
         shareType: "shared",
-        ref: `I2:I${data.length + 1}`, // check if this is off by one
+        ref: `J2:J${data.length + 1}`, // check if this is off by one
     };
 
     // then cascade the formula down
     // this part will probably crash if data is empty
     for (let index = 1; index < data.length; index++) {
-        worksheet.getCell(`I${index + 2}`).value = { sharedFormula: "I2" };
+        worksheet.getCell(`J${index + 2}`).value = { sharedFormula: "J2" };
     }
 
     // apply conditional formatting
+    worksheet.addConditionalFormatting({
+        ref: `A2:J${data.length + 1}`,
+        rules: [
+            {
+                type: "expression",
+                formulae: [ '=OR($I2="报废", $I2="弃置")' ],
+                style: {fill: {type: 'pattern', pattern: 'solid', bgColor: {argb: 'FF6F6F7A'}}},
+            }
+        ],
+    });
 
     // send off the workbook
     workbook.xlsx.write(stream);
